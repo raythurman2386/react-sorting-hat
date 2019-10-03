3d4c7dc02c552ba2e7e763605b3d8b491a2ad92f
raythurman2386 <raymondthurman5@gmail.com>
2019-10-02 20:33:10 -0400

    created houseView

--- react-sorting-hat/src/App.js
+++ react-sorting-hat/src/App.js
@@ -1,6 +1,25 @@
 import React, { Component } from 'react'
 
+// Import my house data
+import {
+  griffindorData,
+  hufflepuffData,
+  ravenclawData,
+  slytherinData,
+} from './data'
+
 class App extends Component {
+  // constructor for state
+  constructor() {
+    super()
+    this.state = {
+      gryffindor: griffindorData,
+      hufflepuff: hufflepuffData,
+      ravenclaw: ravenclawData,
+      slytherin: slytherinData,
+    }
+  }
+
   render() {
     return (
       <div className='App'>
