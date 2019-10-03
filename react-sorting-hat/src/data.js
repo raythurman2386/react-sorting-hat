14cf71e29cfa3a08e0979749b9d169a0216bc5bc
raythurman2386 <raymondthurman5@gmail.com>
2019-10-02 21:42:59 -0400

    updated data to include common room

--- react-sorting-hat/src/data.js
+++ react-sorting-hat/src/data.js
@@ -12,6 +12,7 @@ export const griffindorData = {
   head: 'Minerva McGonagall',
   ghost: 'Nearly Headless Nick',
   crest: Gryffindor,
+  common_room: 'Gryffindor Tower',
   description: `Gryffindor corresponds to the element of Fire. The common room was located in one of the highest towers at Hogwarts, the entrance was situated on the seventh floor in the east wing of the castle and is guarded by a portrait of The Fat Lady. She permits entrance if given the correct password, which is changed numerous times throughout the school year. Famous Gryffindors include Albus Dumbledore, Harry Potter, and Minervra McGonagall.`,
 }
 
@@ -24,6 +25,7 @@ export const hufflepuffData = {
   head: 'Pomona Sprout',
   ghost: 'Fat Friar',
   crest: Hufflepuff,
+  common_room: 'Hufflepuff Basement',
   description: `Hufflepuff corresponds to the element of earth. The Hufflepuff dormitories and common room are located somewhere in the basement, near the castle's kitchens.It can be accessed by tapping the barrel two from the bottom, middle of the second row in the rhythm of "Helga Hufflepuff" and is described as being a cosy and welcoming place with yellow hangings, fat armchairs, and underground tunnels that lead to the dormitories, which have perfectly round doors, similar to barrel tops.Famous Hufflepuffs include Hengist of Woodcroft(founder of Hogsmeade), Newt Scamander(author of Fantastic Beasts and Where to Find Them), and Artemisia Lufkin(first female minister for magic).`,
 }
 
@@ -35,6 +37,7 @@ export const ravenclawData = {
   head: 'Filius Flitwick',
   ghost: 'Grey Lady',
   crest: Ravenclaw,
+  common_room: 'Ravenclaw Tower',
   description: `Ravenclaw corresponds to the element of air. The Ravenclaw common room and dormitories are located in a tower on the west side of the castle. Ravenclaw students must answer a riddle as opposed to giving a password to enter their dormitories. This riddle, however, can be answered by non-Ravenclaws. Famous Ravenclaws include Gilderoy Lockheart, Ignatia Wildsmith (inventor of Floo powder), and Garrick Ollivander. It is roumored that Alastor "Mad-Eye" Moody was a Ravenclaw.`,
 }
 
@@ -46,5 +49,6 @@ export const slytherinData = {
   head: 'Serverus Snape',
   ghost: 'Bloody Baron',
   crest: Slytherin,
+  common_room: 'Slytherin Dungeon',
   description: `Slytherin corresponds roughly to the element of water. The Slytherin dormitories and common room are reached through a bare stone wall in the Dungeons. The Slytherin common room lies beneath the Black Lake. It is a long, low underground room with rough stone walls and silver lamps hanging from the ceiling. Famous Slytherins include Merlin, Tom Riddle, and Dolores Umbridge.`,
 }
