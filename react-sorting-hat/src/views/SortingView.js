f25b507ebf8f4b85f10f13a1b7a8fba419309698
raythurman2386 <raymondthurman5@gmail.com>
2019-10-06 19:19:31 -0400

    added the image to sorting hat view, may not stay

--- react-sorting-hat/src/views/SortingView.js
+++ react-sorting-hat/src/views/SortingView.js
@@ -1,9 +1,12 @@
 import React, { useState } from 'react'
 
+import Hat from '../assets/the_hat.png'
+
 const SortingView = () => {
   return (
     <div>
       <h1>Sorting hat view</h1>
+      <img src={Hat} alt='The Sorting Hat' />
     </div>
   )
 }
