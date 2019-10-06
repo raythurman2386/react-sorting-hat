9c91102946bf6396b7ab756e8b328b1e3b90bdcb
raythurman2386 <raymondthurman5@gmail.com>
2019-10-05 21:52:52 -0400

    added initial link styles

--- react-sorting-hat/src/views/Home.js
+++ react-sorting-hat/src/views/Home.js
@@ -1,14 +1,35 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 import Hogwarts from '../assets/the_sorting_hat.png'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Sorting Hat</h1>
+    <HomeWrapper>
+      <h1>Welcome to Hogwarts</h1>
       <img src={Hogwarts} alt='Hogwarts' />
-    </div>
+      <StyledLink to='#'>Click to be Sorted</StyledLink>
+    </HomeWrapper>
   )
 }
 
 export default Home
+
+const HomeWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 98%;
+  opacity: 0.8;
+  z-index: 1;
+`
+const StyledLink = styled(Link)`
+  padding: 5px 12px;
+  width: 250px;
+  background-color: white;
+  border-radius: 5px;
+  border: none;
+  text-decoration: none;
+  text-align: center;
+`
